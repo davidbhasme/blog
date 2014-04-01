de82107e62659124c7e7bb4d8b9675c8f222c693
David Bhasme <dravidauchkalaranga@gmail.com>
2014-04-01 07:28:29 -0700

    Post and comment functioning
    created post and comment scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '7e90cf6f39c0b0e29d0ca3ec30fc7b8114f2bb4c3a90b980756517104facdda6e88fa112dadced0396789253ca07988671c6fadea99bceffd522133cd1a40a1b'
